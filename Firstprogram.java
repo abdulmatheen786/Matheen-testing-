1ef698ac48753f21aac1cf90b7f4084d630d8a54
Abdul Matheen <shaikmatheen2468@gmail.com>
2025-06-06 19:35:50 +0530

    Add files via upload

---
+++ Firstprogram.java
@@ -0,0 +1,10 @@
+package myfirstpackage;
+
+public class Firstprogram {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
+	}
+
+}
---
+++ markscalculator.java
@@ -0,0 +1,24 @@
+package myfirstpackage;
+
+public class markscalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		        // Store marks of 3 subjects
+		        int math = 99;
+		        int science = 90;
+		        int english = 85;
+
+		        // Calculate total marks
+		        int total = math + science + english;
+
+		        // Calculate average marks
+		        double average = total / 3.0;
+
+		        // Print total and average
+		        System.out.println("Total marks: " + total);
+		        System.out.println("Average marks: " + average);
+		    }
+		}
+
